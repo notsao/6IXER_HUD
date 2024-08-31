8f58e10510852bd8018cbe511c6d98f03030a5dc
notsao <159499062+notsao@users.noreply.github.com>
2024-08-31 22:35:48 +0100

    made more changes

---
+++ 6ixer_ui/html/6ixer_ui/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+lua54 'yes'
